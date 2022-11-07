14c53f772b7b76d8bb2224ccb8fafcfde49089e4
German Dojcinovic <germandoicinovic@gmail.com>
2022-11-07 15:41:29 +0100

    netlify build failed, modifyed vite config and trying again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       // backgroundImage: {
       //   hero: "url('./assets/space_background.png')",
       // },
-    },
+    }
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require('@tailwindcss/forms')]
 };
